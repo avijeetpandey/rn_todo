8ce6c717901cf712eaa5e97609b1a170ed0a1051
Avijeet Pandey <avijeetpandey25@gmail.com>
2022-10-08 23:00:43 +0530

    Add : input field

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {SafeAreaView, StyleSheet, Text, TextInput, View} from 'react-native';
 
 const COLORS = {
-  primary: '#1f145c',
+  primary: '#262626',
+  secondary: '#757575',
   white: '#ffffff',
 };
 
@@ -13,6 +14,15 @@ const App = () => {
       <View style={styles.heading}>
         <Text style={styles.text}>TODO APP</Text>
         <Icon name="delete" color="red" size={25} />
+      </View>
+      <View style={styles.footer}>
+        <View style={styles.inputContainer}>
+          <TextInput
+            placeholder="Add Todo"
+            placeholderTextColor={COLORS.secondary}
+            style={{color: COLORS.primary}}
+          />
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -33,6 +43,25 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 20,
     color: COLORS.primary,
+  },
+  footer: {
+    position: 'absolute',
+    bottom: 0,
+    color: COLORS.white,
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+  inputContainer: {
+    elevation: 40,
+    backgroundColor: COLORS.white,
+    flex: 1,
+    height: 50,
+    marginVertical: 20,
+    marginRight: 20,
+    paddingHorizontal: 20,
+    borderRadius: 30,
   },
 });
 

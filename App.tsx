282e8312469c8b829b0558289879f8fc7fdbd209
Avijeet Pandey <avijeetpandey25@gmail.com>
2022-10-08 22:47:14 +0530

    Add : App header

--- App.tsx
+++ App.tsx
@@ -1,12 +1,39 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+
+const COLORS = {
+  primary: '#1f145c',
+  white: '#ffffff',
+};
 
 const App = () => {
   return (
-    <View>
-      <Text>RN to do app</Text>
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.heading}>
+        <Text style={styles.text}>TODO APP</Text>
+        <Icon name="delete" color="red" size={25} />
+      </View>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: COLORS.white,
+  },
+  heading: {
+    padding: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  text: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    color: COLORS.primary,
+  },
+});
 
 export default App;
